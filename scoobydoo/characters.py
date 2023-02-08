b45590a8afbc217286820c710aa6250331b5ffdf
kyle-woodward <kw.geospatial@gmail.com>
2023-02-08 16:38:36 -0500

    improvements

--- scoobydoo/characters.py
+++ scoobydoo/characters.py
@@ -1,19 +1,29 @@
 import random
 
-class people:
-    kind = 'human'         # class variable shared by all instances
-
-    def __init__(self, name):
-        self.name = name    # instance variable unique to each instance
-
-shaggy = people('shaggy')
+class Character:
+    def __init__(self,name):
+        self.name=name
     
-class animals:
-    kind = 'dog'
-
-    def __init__(self,name:str,phrases:list):
-        self.phrases = {name:phrases}
-
     def gimme_a_catchphrase(self,n):
         phrase_list = self.phrases[n]
         return random.choice(phrase_list)
+
+# define child class of Character
+class People(Character):
+    kind = 'human'         # class variable shared by all instances
+
+    def __init__(self, name, shirtcolor,gender):
+        super(Character, self).__init__(name)    # instance variable unique to each instance
+        self.shirtcolor = shirtcolor
+        self.gender = gender
+
+shaggy = People('shaggy')
+    
+class Animals(Character):
+    def __init__(self, name, species):
+        super(Character, self).__init__(name)    # instance variable unique to each instance
+        self.species = species
+
+class Villains(Character):
+    def __init__(self,name,unmasked=False):
+        super(Character, self).__init__(name)
